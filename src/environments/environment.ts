3473e46197e3ed525c5bdb695f3ebe44dbfc692c
Sara Bisheer <113303945+SH8664@users.noreply.github.com>
2026-05-20 18:49:43 +0300

    change visual search key and model

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,16 @@ export const environment = {
   production: false,
   apiBaseUrl: 'https://fakestoreapiserver.reactbd.org/api/products',
   supabaseUrl: 'https://xckhkeelnhlalcckuwkn.supabase.co',
-  supabaseKey: 'SUPABASE_ANON_KEY',
+  supabaseKey:
+    'SUPABASE_ANON_KEY',
   HuggingFaceApiKey: 'HF_API_KEY',
-  MistralApiKey: 'MISTRAL_API_KEY_2',
+  MistralApiKey: 'MISTRAL_API_KEY',
   MistralModel: 'mistral-large-latest',
   MistralApiUrl: 'https://api.mistral.ai/v1/chat/completions',
   // MistralApiKey: 'MISTRAL_API_KEY_2',
-  ImageModel: 'mistral-small-2603',
+  ImageModel: 'mistral-medium-3.5',
   TextModel: 'mistral-large-latest',
   paymentApiUrl: 'http://localhost:5199/api/payments',
-  stripePublicKey: 'STRIPE_PUBLIC_KEY'
+  stripePublicKey:
+    'STRIPE_PUBLIC_KEY',
 };
